e8ac47ac764bcf0db61425fec266f656e8e85a98
Joseph <jose>
2020-01-07 16:38:32 -0600

    delete some folders

--- RNChat/src/reducers/connection.js
+++
@@ -1,9 +0,0 @@
-import { CHAT_CONNECTED, CHAT_DISCONNECTED } from '../actions/connection'
-
-export default (connected = false, action) => {
-	switch (action.type) {
-		case CHAT_CONNECTED: return true
-    case CHAT_DISCONNECTED: return false
-		default: return connected
-	}
-}
